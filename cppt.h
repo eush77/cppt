65ad2162934eb6fce951a063314a905bf3fb18d8
Eugene Sharygin <eush77@gmail.com>
2015-01-31 11:20:14 +0300

    Add built-in macrodefinitions

---
+++ cppt.h
@@ -0,0 +1,3 @@
+#define t(tag, attributes, body) "<" tag attributes ">" body "</" tag ">"
+
+#define a(key, value) " " key "=\"" value "\""
--- driver.c
+++ driver.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "cppt.h"
 #include "template.cppt"
 
 int main() {
